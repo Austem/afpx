94bae9f037471d6eea63e5d6d05afd4dd4e6c775
victor.ferzar@gmail.com <viikthor18>
2020-12-20 17:25:05 -0300

    mostrando la palabra en pantalla y transformandola en secretCode

--- resources/views/typingw.blade.php
+++ resources/views/typingw.blade.php
@@ -1,40 +1,49 @@
 @extends('layouts.master')
 
 @section('content')
+
     <div class="container-fluid bg-primary h-full">
         <div class="row">
             <div class="col-3">
                 Score:
             </div>
+            <?php foreach ($palabras as $palabra) {?>
             <div class="col-6">
                 <div class="form-group border text-center">
-                    Palabra
+                    <?php echo $palabra ?>
                     <br>
                     -Definicion de la palabra
-                    <div>
+                    <div class="flex-row justify-content-center">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email" class="form-control col-3 p-2" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
                 </div>
             </div>
+            <?php }?>
             <div class="col-3">
                 Vidas:
             </div>
         </div>
     </div>
+
 @endsection
 
 @section('scripts')
     <script type="text/javascript" src="https://www.cornify.com/js/cornify.js"></script>
 
     <script>
+
+        let palabra = <?php echo json_encode($palabras)?>;
+        console.log(palabra[0].toLowerCase());
         const pressed = [];
-        const secretCode = 'pletorico';
+        const secretCode = palabra[0].toLowerCase();
+
+
 
         window.addEventListener('keyup', (e) => {
 
             console.log(e.key);
-            pressed.push(e.key);
+            pressed.push(e.key.toLowerCase());
 
             //El método splice() cambia el contenido de un array eliminando elementos existentes
             // y/o agregando nuevos elementos.
